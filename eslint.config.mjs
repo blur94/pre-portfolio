fc68cd150b70d2c77e940792ad94b72571558b1c
Gilead Odo <odogilead@gmail.com>
2026-05-22 17:44:34 +0100

    chore: finish eslint cli setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,10 +5,13 @@ import nextTs from "eslint-config-next/typescript";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
-  {
-    ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"]
-  }
+  globalIgnores([
+    ".gstack/**",
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+  ]),
 ]);
 
 export default eslintConfig;
